a0be82f4fd454a29cdfd0407a7ccd2f4c1e91bb0
AnikaTabassum <35559892+AnikaTabassum@users.noreply.github.com>
2019-04-09 19:09:41 +0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
 
 include('header.php');
-include('menu.php');
 ?>
 <div id="main_content">
 		<div id="body_content">Hello world from Home Page</div>
-		<?php include('advert.php'); ?>
+		
 </div>
 <?php
 include('footer.php');
