8d4f17a22f1236b85d6eb5cbd04cadcb565599ae
AnikaTabassum <35559892+AnikaTabassum@users.noreply.github.com>
2019-04-09 19:07:28 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+include('header.php');
+include('menu.php');
+?>
+<div id="main_content">
+		<div id="body_content">Hello world from Home Page</div>
+		<?php include('advert.php'); ?>
+</div>
+<?php
+include('footer.php');
+?>
